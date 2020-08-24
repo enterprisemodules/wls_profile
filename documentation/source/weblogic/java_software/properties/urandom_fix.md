b47fb698546534420728bbdfb950637b4b7549d1
Bert Hajee <bert.hajee@enterprisemodules.com>
2020-08-24 17:48:10 +0200

    [java_software] Start using module puppetlabs-java

--- documentation/source/weblogic/java_software/properties/urandom_fix.md
+++ documentation/source/weblogic/java_software/properties/urandom_fix.md
@@ -1,3 +1,5 @@
 Enable the urandom fix.
 
+This parameter is deprecated and will be removed in a next version.
+
 Default value: `true`
--- manifests/weblogic/java_software.pp
+++ manifests/weblogic/java_software.pp
@@ -27,11 +27,12 @@
 #    This value is used in multiple places. To make sure in all classed the correct value is used, use the hiera key `wls_profile::source` to change it to your requested value.
 #    The default is : `puppet:///modules/software/`
 #
-# @param [Boolean] urandom_fix
+# @param [Optional[Boolean]] urandom_fix
 #    Enable the urandom fix.
+#    This parameter is deprecated and will be removed in a next version.
 #    Default value: `true`
 #
-# @param [Boolean] rsa_key_size_fix
+# @param [Optional[Boolean]] rsa_key_size_fix
 #    Enable the RSA keysize fix.
 #    Default value: `true`
 #
@@ -39,22 +40,34 @@
 #    Install x64 version of java.
 #    Default value: `true`
 #
-# @param [Integer] alternatives_priority
+# @param [Optional[Integer]] alternatives_priority
 #    alternatives priority for jdk.
 #    Default value is: `18001`
 #
 #--++--
 # lint:ignore:variable_scope
 class wls_profile::weblogic::java_software(
-  String  $version,
-  String  $full_version,
-  String  $cryptography_extension_file,
-  String  $source,
-  Boolean $urandom_fix,
-  Boolean $rsa_key_size_fix,
-  Boolean $x64,
-  Integer $alternatives_priority,
+  String            $version,
+  String            $full_version,
+  String            $cryptography_extension_file,
+  String            $source,
+  Optional[Boolean] $urandom_fix,
+  Optional[Boolean] $rsa_key_size_fix,
+  Boolean           $x64,
+  Optional[Integer] $alternatives_priority,
 ) inherits wls_profile {
+
+  if $urandom_fix != undef {
+    deprecation (urandom_fix, "Parameter 'wls_profile::weblogic::java_software::urandom_fix' is deprecated and will be removed in a future release")
+  }
+
+  if $alternatives_priority != undef {
+    deprecation (alternatives_priority, "Parameter 'wls_profile::weblogic::java_software::alternatives_priority' is deprecated and will be removed in a future release")
+  }
+
+  if $rsa_key_size_fix != undef {
+    deprecation (rsa_key_size_fix, "Parameter 'wls_profile::weblogic::java_software::rsa_key_size_fix' is deprecated and will be removed in a future release")
+  }
 
   echo {"Java version ${version}":
     withpath => false
@@ -80,16 +93,20 @@ class wls_profile::weblogic::java_software(
         ensure  => absent,
       }
 
-      ->jdk7::install7{ "jdk-${version}-linux-x64":
-        version                     => $version ,
-        full_version                => $full_version,
-        alternatives_priority       => $alternatives_priority,
-        x64                         => $x64,
-        download_dir                => $download_dir,
-        urandom_java_fix            => $urandom_fix,
-        rsa_key_size_fix            => $rsa_key_size_fix,
-        cryptography_extension_file => $cryptography_extension_file,
-        source_path                 => $source,
+      -> java::download{ "jdk-${version}-linux-x64.tar.gz":
+        ensure         => 'present',
+        java_se        => 'jdk',
+        version_major  => $version,
+        version_minor  => '',
+        url            => "${source}/jdk-${version}-linux-x64.tar.gz",
+        package_type   => 'tar.gz',
+        jce            => true,
+        jce_url        => "${source}/${cryptography_extension_file}",
+        manage_basedir => true,
+      }
+      -> file {'/usr/bin/java':
+        ensure => 'link',
+        target => "/usr/java/${full_version}/bin/java"
       }
     }
     'Solaris': {
